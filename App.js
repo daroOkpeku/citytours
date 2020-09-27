ee2e72e58967a6bea25413fcb8ce8bf25e40d2b5
stephen okpeku <stephenjason41@gmail.com>
2020-09-27 18:58:07 +0100

    controllable form

--- App.js
+++ App.js
@@ -1,55 +1,61 @@
 import React, { Component } from 'react'
-import PropTypes from "prop-types"
+import "./App.css"
+/*Controllable form  */
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Person />
-      </div>
-    )
-  }
-}
 
-
-
-class Person extends Component {
   state = {
-    people: [
-      {  /* name: "john", img: "https://randomuser.me/api/portraits/thumb/men/69.jpg", */ age: 20, id: 1 },
-      { name: "wick", img: "https://randomuser.me/api/portraits/thumb/men/90.jpg", age: 22, id: 2 },
-      {/* img: "https://randomuser.me/api/portraits/thumb/men/41.jpg", */ name: "jack", age: 20, id: 3 }
-    ]
+    firstName: "",
+    lastName: "",
+    people: []
+
+  }
+  /* word = (event) => {
+      console.log(event.target.name);
+      console.log(event.target.value);
+      if (event.target.name === "firstName") {
+        const text = event.target.value;
+        this.setState({
+          firstName: text,
+        })
+      }
+  
+    } */
+
+  word = (event) => {
+    this.setState({
+      [event.target.name]: [event.target.value]
+    })
+  }
+  display = (e) => {
+    e.preventDefault();
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+    if (firstName.length > 0 && lastName.length > 0) {
+
+      const person = `${firstName} ${lastName}`;
+      this.setState({
+        people: [...this.state.people, person],
+        firstName: "",
+        lastName: ""
+
+      })
+    }
   }
 
   render() {
+
     return (
       <div>
-        {this.state.people.map((item) => <Persons name={item.name} img={item.img} age={item.age} key={item.id} />)}
+        <form onSubmit={this.display}>
+          <input type="text" name="firstName" value={this.state.firstName} onChange={this.word} />
+          <input type="text" name="lastName" value={this.state.lastName} onChange={this.word} />
+          <button type="submit" >click me</button>
+        </form>
+        <span>
+          people
+        <section>{this.state.people}</section>
+        </span>
       </div>
     )
   }
-}
-
-Persons.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-}
-
-Persons.defaultProps = {
-  name: "steve",
-  age: 24,
-  img: "https://randomuser.me/api/portraits/thumb/men/51.jpg",
-
-}
-
-
-function Persons({ name, img, age }) {
-  return (
-    <section>
-      <img src={img} />
-      <h2>{name}</h2>
-      <h3>{age}</h3>
-    </section>
-  )
 }
